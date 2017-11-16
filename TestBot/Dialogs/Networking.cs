8337d95ec9ebf718f843159cfecdf7147e342ef1
Adrian <adrian.edlund@creuna.no>
2017-11-16 11:57:38 +0100

    Refactor Networking class, keeping functionality. Post message if response from Wit is empty

--- TestBot/Dialogs/Networking.cs
+++ TestBot/Dialogs/Networking.cs
@@ -1,74 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace TestBot.Dialogs
 {
     public class Networking
     {
-        public string response { get; set; }
-        public string[] responseEntities { get; set; }
-        public Networking()
-        {
+        private const string AccessToken = "YHOYLGOGY2ZF2ONV465TXJDD4RBL5OC2";
 
-        }
-        public void ConnectToWit(string message)
+        public string GetResponseForMessage(string message)
         {
-            string URL = $"https://api.wit.ai/message?v={DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year}&q={message}";
-            Console.WriteLine(URL);
-            string accessToken = "YHOYLGOGY2ZF2ONV465TXJDD4RBL5OC2";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            //TODO: fjern denne. Kun for å teste at GetEntities fortsatt fungerer
+            GetEntities().ForEach(Console.WriteLine);
+
+            if (string.IsNullOrEmpty(message))
+                return string.Empty;
+            var url = CreateUrlToWit(message, DateTime.UtcNow);
+            Console.WriteLine(url);
+            var request = CreatePostRequest(url);
+            try
+            {
+                return GetResponse(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return string.Empty;
+            }
+       }
+
+        private static string CreateUrlToWit(string message, DateTime timeNow)
+        {
+            return $"https://api.wit.ai/message?v={timeNow.Day}.{timeNow.Month}.{timeNow.Year}&q={message}";
+        }
+
+        private static string CreateEntitiesUrlToWit(DateTime timeNow)
+        {
+            return $"https://api.wit.ai/entities?v={timeNow.Day}.{timeNow.Month}.{timeNow.Year}";
+        }
+
+        private static HttpWebRequest CreatePostRequest(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
             request.PreAuthenticate = true;
-            request.Headers.Add("Authorization", "Bearer " + accessToken);
+            request.Headers.Add("Authorization", "Bearer " + AccessToken);
             request.Accept = "application/json";
-
-            var response = request.GetResponse();
-            var responseStream = response.GetResponseStream();
-            if (responseStream == null)
-            {
-                Console.WriteLine("ER NULL");
-            }
-            else
-            {
-                var reader = new StreamReader(responseStream);
-                this.response = reader.ReadToEnd();
-                Console.WriteLine(reader.ReadToEnd());
-            }
+            return request;
         }
-        public void GetEntities()
+
+        private static HttpWebRequest CreateGetRequest(string url)
         {
-            string URL = $"https://api.wit.ai/entities?v={DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year}";
-            string accessToken = "YHOYLGOGY2ZF2ONV465TXJDD4RBL5OC2";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            var request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "GET";
             request.PreAuthenticate = true;
-            request.Headers.Add("Authorization", "Bearer " + accessToken);
+            request.Headers.Add("Authorization", "Bearer " + AccessToken);
             request.Accept = "application/json";
+            return request;
+        }
 
-            var response = request.GetResponse();
-            var responseStream = response.GetResponseStream();
+        private static string GetResponse(HttpWebRequest request)
+        {
+            var responseStream = request
+                .GetResponse()
+                .GetResponseStream();
             if (responseStream == null)
+                return string.Empty;
+
+            using (var reader = new StreamReader(responseStream))
             {
-                Console.WriteLine("ER NULL");
+                return reader.ReadToEnd();
             }
-            else
+        }
+
+        public List<string> GetEntities()
+        {
+            var url = CreateEntitiesUrlToWit(DateTime.UtcNow);
+            var request = CreateGetRequest(url);
+            try
             {
-                var reader = new StreamReader(responseStream);
-                var entityString = reader.ReadToEnd();
-                var temp = entityString.Replace("\"", "").Replace("[", "").Replace("]", "");
-                this.responseEntities = temp.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-                var finalArray = new List<string>();
-                foreach (var item in this.responseEntities)
-                {
-                    if (!item.Contains("$"))
-                    {
-                        finalArray.Add(item);
-                    }
-                }
-                finalArray.ForEach(a => Console.WriteLine(a));
+                var response = GetResponse(request);
+                var entities = GetEntitiesFromResponse(response);
+                return entities?.ToList() ?? new List<string>();
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new List<string>();
+            }
+        }
+
+        private static IEnumerable<string> GetEntitiesFromResponse(string entityString)
+        {
+            if(string.IsNullOrEmpty(entityString))
+                return  new List<string>();
+            var trimmedEntityString = entityString
+                .Replace("\"", "")
+                .Replace("[", "")
+                .Replace("]", "");
+            var entities = trimmedEntityString
+                .Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries);
+            return entities
+                .Where(e => !e.Contains("$"));
         }
     }
 }
